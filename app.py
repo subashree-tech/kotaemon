838f5fe970afcd883a613ed8a6a0a899f9dde2f0
subashree-tech <subashree@klmsolutions.in>
2024-09-04 20:03:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+import sys
 import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from theflow.settings import settings as flowsettings
-
 KH_APP_DATA_DIR = getattr(flowsettings, "KH_APP_DATA_DIR", ".")
 GRADIO_TEMP_DIR = os.getenv("GRADIO_TEMP_DIR", None)
 # override GRADIO_TEMP_DIR if it's not set
@@ -9,9 +10,7 @@ if GRADIO_TEMP_DIR is None:
     GRADIO_TEMP_DIR = os.path.join(KH_APP_DATA_DIR, "gradio_tmp")
     os.environ["GRADIO_TEMP_DIR"] = GRADIO_TEMP_DIR
 
-
 from ktem.main import App  # noqa
-
 app = App()
 demo = app.make()
 demo.queue().launch(
